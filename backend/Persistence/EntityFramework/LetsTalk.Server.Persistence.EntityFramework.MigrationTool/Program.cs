e3fec2a37576e683698962afcabd15d5a5d81564
Evgenii Petukhov <evgenii.petukhov@gameteq.com>
2026-08-28 05:26:35 +0200

    Introduced a new project for sql migrations

---
+++ backend/Persistence/EntityFramework/LetsTalk.Server.Persistence.EntityFramework.MigrationTool/Program.cs
@@ -0,0 +1,43 @@
+﻿using LetsTalk.Server.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.Globalization;
+
+using var host = CreateDefaultBuilder().Build();
+await host.RunAsync();
+
+// https://thecodeblogger.com/2021/05/04/how-to-use-appsettings-json-config-file-with-net-console-applications/
+static IHostBuilder CreateDefaultBuilder()
+{
+    return Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(app =>
+        {
+            var filename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
+            app.AddJsonFile(filename, optional: false);
+        })
+        .ConfigureServices((context, services) =>
+        {
+            services.AddDbContext<LetsTalkDbContext>(options =>
+            {
+                var connectionString = context.Configuration.GetConnectionString("MySql")!;
+                options
+                    .UseMySQL(
+                        connectionString,
+                        mysqlOptions =>
+                        {
+                            mysqlOptions.MigrationsHistoryTable("__efmigrationshistory");
+                        })
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        })
+        .UseSerilog((context, loggerConfig) =>
+        {
+            loggerConfig
+                .ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture);
+        });
+}
